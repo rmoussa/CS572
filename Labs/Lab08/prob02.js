41219bd6a476822d939124be499bce52c60d05e3
Ryan <ryan.mousa14@gmail.com>
2018-10-10 18:55:13 -0500

    Exrecise 2

---
+++ Labs/Lab08/prob02.js
@@ -0,0 +1,24 @@
+{
+    "_id": "5bbbb564dd4eb69795fb4f5e",
+    "ISBN": "105122",
+    "bookName": "Node.js",
+    "author": "Ryan :)",
+    "tags": [
+       "MWA",
+       "Programming"
+    ],
+    "borrowedBy": {
+       "stdName": "Ahmed",
+       "stdId": "1",
+       "startDate": "10/10/2018",
+       "endDate": "11/10/2018",
+       "phoneNumber": "6416416411"
+    }
+ }
+ 
+ Create 3 indexes over the book collection one index for the following properties:
+  - ISBN
+  - Author
+  - Tags
+  And another indexes for borrowedBy for properties:
+  - endDate+phoneNumber
